b80c4cc447b1c75866a593a960ac4329d1bb3957
Kris <Wellingking@users.noreply.github.com>
2018-04-24 22:41:44 +0800

    update

--- clone.py
+++ clone.py
@@ -1,9 +1,9 @@
 import csv
 import cv2
 import numpy as np
-
+path = '../carnd-behavioral-cloning/data_02/'
 lines = []
-with open('../carnd-behavioral-cloning/driving_log.csv') as csvfile:
+with open(path + 'driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -12,24 +12,27 @@ images = []
 measurements = []
 for line in lines:
     source_path = line[0]
-    filename = source_path.split('\\')[-1]
-    current_path = '../carnd-behavioral-cloning/IMG/' + filename
+    filename = source_path.split('\\')[-1] #source_path.split('/')[-1] #
+    current_path = path + 'IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
-    measurements.append(measurement)
+    measurements.append(measurement)    
+    #print(current_path)
     
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
 
 model.save('model.h5')
+#exit()
