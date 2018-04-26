445c149df6894be228b44e666ac73c2bb04b0bd6
Kris <Wellingking@users.noreply.github.com>
2018-04-27 00:02:55 +0800

    augmentation update

--- clone.py
+++ clone.py
@@ -1,7 +1,7 @@
 import csv
 import cv2
 import numpy as np
-path = '../carnd-behavioral-cloning/data_02/'
+path = '../carnd-behavioral-cloning/data_02/' #'../carnd-behavioral-cloning/data_02/'
 lines = []
 with open(path + 'driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -18,21 +18,38 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)    
-    #print(current_path)
+    #print(current_path)    
+
+
+
+
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement * -1.0)
     
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse',optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
 model.save('model.h5')
 #exit()
